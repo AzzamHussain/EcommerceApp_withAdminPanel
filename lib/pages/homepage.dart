3a5032933eff8fbd8988f33d10c6c4b60fa54bd7
AzzamHussain <azzamhussain70@gmail.com>
2024-08-10 02:26:37 +0500

    building HomePage

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -15,6 +15,7 @@ class _HomepageState extends State<Homepage> {
       body: Container(
         margin: EdgeInsets.only(top: 50.0, left: 20.0, right: 10.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -36,13 +37,48 @@ class _HomepageState extends State<Homepage> {
             SizedBox(
               height: 30.0,
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text(
-                  "Delicious Food",
-                  style: AppWidget.HeadLineTextFieldStyle(),
-                  textAlign: TextAlign.right,
+                RichText(
+                  textAlign: TextAlign.left,
+                  text: TextSpan(
+                    children: [
+                      TextSpan(
+                        text: "Delicious Food\n",
+                        style: AppWidget.HeadLineTextFieldStyle(),
+                      ),
+                      TextSpan(
+                        text: "Discover and Get Food",
+                        style: AppWidget.LightTextFieldStyle(),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 10), // Space between the text and images
+                Row(
+                  children: [
+                    Image.asset(
+                      'assets/burger.png',
+                      height: 40,
+                      width: 40,
+                      fit: BoxFit.cover,
+                    ),
+                    SizedBox(width: 15),
+                    Image.asset(
+                      'assets/pizza.png',
+                      height: 40,
+                      width: 40,
+                      fit: BoxFit.cover,
+                    ),
+                    SizedBox(width: 10),
+                    Image.asset(
+                      'assets/ice-cream.png',
+                      height: 40,
+                      width: 40,
+                      fit: BoxFit.cover,
+                    ),
+                  ],
                 ),
               ],
             ),
--- lib/widgets/widget_support.dart
+++ lib/widgets/widget_support.dart
@@ -3,11 +3,25 @@ import 'package:flutter/material.dart';
 class AppWidget {
   static TextStyle boldTextFieldStyle() {
     return TextStyle(
-        fontSize: 20, fontWeight: FontWeight.bold, fontFamily: 'Poppins');
+        color: Colors.black,
+        fontSize: 20,
+        fontWeight: FontWeight.bold,
+        fontFamily: 'Poppins');
   }
 
   static TextStyle HeadLineTextFieldStyle() {
     return TextStyle(
-        fontSize: 24.0, fontWeight: FontWeight.bold, fontFamily: 'Poppins');
+        color: Colors.black,
+        fontSize: 24.0,
+        fontWeight: FontWeight.bold,
+        fontFamily: 'Poppins');
+  }
+
+  static TextStyle LightTextFieldStyle() {
+    return TextStyle(
+        color: Colors.black38,
+        fontSize: 15.0,
+        fontWeight: FontWeight.w500,
+        fontFamily: 'Poppins');
   }
 }
