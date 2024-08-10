8d9871eac79ab1abeaa9931039566813439f61a1
AzzamHussain <azzamhussain70@gmail.com>
2024-08-10 16:38:35 +0500

    homepage

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -9,6 +9,8 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
+  bool icecream = false, salad = false, burger = false, pizza = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,58 +33,93 @@ class _HomepageState extends State<Homepage> {
                     Icons.shopping_cart_outlined,
                     color: Colors.white,
                   ),
-                )
+                ),
               ],
             ),
             SizedBox(
               height: 30.0,
             ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                RichText(
-                  textAlign: TextAlign.left,
-                  text: TextSpan(
-                    children: [
-                      TextSpan(
-                        text: "Delicious Food\n",
-                        style: AppWidget.HeadLineTextFieldStyle(),
-                      ),
-                      TextSpan(
-                        text: "Discover and Get Food",
-                        style: AppWidget.LightTextFieldStyle(),
-                      ),
-                    ],
+            RichText(
+              textAlign: TextAlign.left,
+              text: TextSpan(
+                children: [
+                  TextSpan(
+                    text: "Delicious Food\n",
+                    style: AppWidget.HeadLineTextFieldStyle(),
                   ),
-                ),
-                SizedBox(height: 10), // Space between the text and images
-                Row(
-                  children: [
-                    Image.asset(
-                      'assets/burger.png',
-                      height: 40,
-                      width: 40,
-                      fit: BoxFit.cover,
-                    ),
-                    SizedBox(width: 15),
-                    Image.asset(
-                      'assets/pizza.png',
-                      height: 40,
-                      width: 40,
-                      fit: BoxFit.cover,
-                    ),
-                    SizedBox(width: 10),
-                    Image.asset(
-                      'assets/ice-cream.png',
-                      height: 40,
-                      width: 40,
-                      fit: BoxFit.cover,
-                    ),
-                  ],
-                ),
+                  TextSpan(
+                    text: "Discover and Get Food",
+                    style: AppWidget.LightTextFieldStyle(),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 10), // Space between the text and images
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                buildSelectableMaterialImage('assets/burger.png', burger, () {
+                  setState(() {
+                    burger = !burger;
+                    pizza = false;
+                    icecream = false;
+                    salad = false;
+                  });
+                }),
+                buildSelectableMaterialImage('assets/pizza.png', pizza, () {
+                  setState(() {
+                    pizza = !pizza;
+                    burger = false;
+                    icecream = false;
+                    salad = false;
+                  });
+                }),
+                buildSelectableMaterialImage('assets/ice-cream.png', icecream,
+                    () {
+                  setState(() {
+                    icecream = !icecream;
+                    burger = false;
+                    pizza = false;
+                    salad = false;
+                  });
+                }),
+                buildSelectableMaterialImage('assets/salad.png', salad, () {
+                  setState(() {
+                    salad = !salad;
+                    burger = false;
+                    pizza = false;
+                    icecream = false;
+                  });
+                }),
               ],
             ),
           ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildSelectableMaterialImage(
+      String assetPath, bool isSelected, VoidCallback onTap) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Material(
+        elevation: isSelected ? 10.0 : 5.0, // Increase elevation if selected
+        borderRadius: BorderRadius.circular(10.0),
+        color: isSelected
+            ? Colors.blue[700]
+            : Colors.white, // Highlight color if selected
+        child: Container(
+          padding: EdgeInsets.all(8.0),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(10.0),
+            child: Image.asset(
+              assetPath,
+              height: 40, // Adjust size as needed
+              width: 40, // Adjust size as needed
+              fit: BoxFit.cover,
+            ),
+          ),
         ),
       ),
     );
