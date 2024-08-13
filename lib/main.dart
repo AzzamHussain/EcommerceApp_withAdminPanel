638b846de6d40d115c19c9a2f1b0043527271cb5
AzzamHussain <azzamhussain70@gmail.com>
2024-08-14 00:28:29 +0500

    detailScreen in progress

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,6 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
       home:BottomNavBar(),
--- lib/pages/details.dart
+++ lib/pages/details.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce_with_adminpanel/widgets/widget_support.dart';
 import 'package:flutter/material.dart';
 
 class Details extends StatefulWidget {
@@ -12,12 +13,56 @@ class _DetailsState extends State<Details> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        margin: EdgeInsets.only(top: 50.0, left: 20.0, right: 20.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Icon(
-              Icons.arrow_back_ios_new_outlined,
-              color: Colors.black,
-            )
+            GestureDetector(
+              onTap: () {
+                Navigator.pop(context);
+              },
+              child: Icon(
+                Icons.arrow_back_ios_new_outlined,
+                color: Colors.black,
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Image.asset(
+              "assets/salad2.png",
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height / 2.5,
+              fit: BoxFit.cover,
+            ),
+            SizedBox(
+              height: 15.0,
+            ),
+          Row(
+  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+  children: [
+    Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          "Russian",
+          style: AppWidget.boldTextFieldStyle(),
+        ),
+        Text(
+          "Salad",
+          style: AppWidget.LightTextFieldStyle(),
+        ),
+      ],
+    ),
+    Container(
+      decoration: BoxDecoration(color: Colors.black),
+      child: Icon(
+        Icons.remove,
+        color: Colors.white,
+      ),
+    ),
+  ],
+)
           ],
         ),
       ),
