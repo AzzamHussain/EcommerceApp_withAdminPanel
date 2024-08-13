aa987a3e6b9e944b5a8803f80ad2c15a96a54eb8
AzzamHussain <azzamhussain70@gmail.com>
2024-08-14 01:48:23 +0500

    detailScreen in progress

--- lib/pages/details.dart
+++ lib/pages/details.dart
@@ -47,11 +47,11 @@ class _DetailsState extends State<Details> {
                   children: [
                     Text(
                       "Russian",
-                      style: AppWidget.boldTextFieldStyle(),
+                      style: AppWidget.HeadLineTextFieldStyle(),
                     ),
                     Text(
                       "Salad",
-                      style: AppWidget.LightTextFieldStyle(),
+                      style: AppWidget.boldTextFieldStyle(),
                     ),
                   ],
                 ),
@@ -97,7 +97,88 @@ class _DetailsState extends State<Details> {
                   ),
                 ),
               ],
-            )
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Text(
+              " A fresh mix of crisp vegetables and flavorful dressing, perfect for a healthy and delicious meal",
+              maxLines: 3,
+              style: AppWidget.LightTextFieldStyle(),
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Row(
+              children: [
+                Text(
+                  "Delivery Time",
+                  style: AppWidget.boldTextFieldStyle(),
+                ),
+                SizedBox(
+                  width: 25.0,
+                ),
+                Icon(
+                  Icons.alarm,
+                  color: Colors.black,
+                ),
+                SizedBox(
+                  width: 5.0,
+                ),
+                Text(
+                  "30 Mins",
+                  style: AppWidget.boldTextFieldStyle(),
+                )
+              ],
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 40.0),
+              child: Row(
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Total price",
+                        style: AppWidget.boldTextFieldStyle(),
+                      ),
+                      Text(
+                        "\$28",
+                        style: AppWidget.boldTextFieldStyle(),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    width: 80.0,
+                  ),
+                  Container(
+                    decoration: BoxDecoration(color: Colors.black),
+                    child: Row(
+                      children: [
+                        Text(
+                          "Add to cart",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16.0,
+                              fontFamily: 'poppins'),
+                        ),
+                        Container(
+                          padding: EdgeInsets.all(3.0),
+                          decoration: BoxDecoration(color: Colors.grey),
+                          child: Icon(
+                            Icons.shopping_cart_outlined,
+                            color: Colors.white,
+                          ),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
           ],
         ),
       ),
