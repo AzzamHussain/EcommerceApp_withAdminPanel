90ebefbe6ede6770999adb2da3c2503b6a7f7898
AzzamHussain <azzamhussain70@gmail.com>
2024-08-17 00:46:39 +0500

    loginScreenin progress

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -40,20 +40,26 @@ class _LoginState extends State<Login> {
             margin: EdgeInsets.only(top: 60.0,left: 20.0,right: 20.0), //ad here left right
             child: Column(
               children: [
-                Image.asset(
-                  "assets/logo.png",
-                  width: MediaQuery.of(context).size.width / 2,
-                  fit: BoxFit.cover,
+                Center(
+                  child: 
+                    Image.asset(
+                      "assets/logo.png",
+                      width: MediaQuery.of(context).size.width / 2,
+                      fit: BoxFit.cover,
+                  
+                    ),),
+                    SizedBox(height: 50.0,),
 
-                ),
-                Container(
-                  width: MediaQuery.of(context).size.width,
-                  decoration: BoxDecoration(color: Colors.white,),
-                  child: Column(children: [
-                    
-                    Text("Login",style: AppWidget.boldTextFieldStyle(),)
-                  ],),
-                ),
+                    Container(
+                      width: MediaQuery.of(context).size.width,
+                      decoration: BoxDecoration(color: Colors.white,),
+                      child: Column(children: [
+                        
+                        Text("Login",style: AppWidget.boldTextFieldStyle(),)
+                      ],),
+                    ),
+              
+                
               ],
             ),
           )
