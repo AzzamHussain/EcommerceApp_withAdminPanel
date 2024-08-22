09f6eabc6a144b7e0cefd1ce10d434f7a435c28b
AzzamHussain <azzamhussain70@gmail.com>
2024-08-23 03:16:10 +0500

    issues with login

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,5 +1,6 @@
 import 'package:ecommerce_with_adminpanel/pages/signup.dart';
 import 'package:ecommerce_with_adminpanel/widgets/widget_support.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class Login extends StatefulWidget {
@@ -10,6 +11,36 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  String email = "", password = "";
+
+  final _formkey = GlobalKey<FormState>();
+
+  TextEditingController useremailcontroller = new TextEditingController();
+  TextEditingController userpasswordcontroller = new TextEditingController();
+
+  UserLogin() async {
+    try {
+      await FirebaseAuth.instance
+          .signInWithEmailAndPassword(email: email, password: password);
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-Found') {
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          content: Text(
+            "No user found for that Email",
+            style: TextStyle(fontSize: 18.0, color: Colors.black),
+          ),
+        ));
+      } else if (e.code == 'Wrong-Password') {
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          content: Text(
+            "Email or Password is incorrect",
+            style: TextStyle(fontSize: 18.0, color: Colors.black),
+          ),
+        ));
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -58,68 +89,103 @@ class _LoginState extends State<Login> {
                     elevation: 5.0,
                     borderRadius: BorderRadius.circular(10.0),
                     child: Container(
-                      padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 30.0), // Added vertical padding
+                      padding: EdgeInsets.symmetric(
+                          horizontal: 20.0,
+                          vertical: 30.0), // Added vertical padding
                       width: MediaQuery.of(context).size.width,
                       decoration: BoxDecoration(
                         color: Colors.white,
                         borderRadius: BorderRadius.circular(10.0),
                       ),
-                      child: Column(
-                        children: [
-                          SizedBox(height: 10.0), // Reduced height for consistency
-                          Text(
-                            "Login",
-                            style: AppWidget.HeadLineTextFieldStyle(),
-                          ),
-                          SizedBox(height: 30.0),
-                          TextField(
-                            decoration: InputDecoration(
-                              hintText: "Email",
-                              hintStyle: AppWidget.boldTextFieldStyle(),
-                              prefixIcon: Icon(Icons.email_outlined),
+                      child: Form(
+                        key: _formkey,
+                        child: Column(
+                          children: [
+                            SizedBox(
+                                height: 10.0), // Reduced height for consistency
+                            Text(
+                              "Login",
+                              style: AppWidget.HeadLineTextFieldStyle(),
                             ),
-                          ),
-                          SizedBox(height: 30.0),
-                          TextField(
-                            decoration: InputDecoration(
-                              hintText: "Password",
-                              hintStyle: AppWidget.boldTextFieldStyle(),
-                              prefixIcon: Icon(Icons.password_outlined),
-                            ),
-                          ),
-                          SizedBox(height: 30.0),
-                          Container(
-                            alignment: Alignment.topRight,
-                            child: Text(
-                              "Forgot password?",
-                              style: AppWidget.boldTextFieldStyle(),
-                            ),
-                          ),
-                          SizedBox(height: 50.0), // Reduced space above the button
-                          Material(
-                            elevation: 5.0,
-                            borderRadius: BorderRadius.circular(20),
-                            child: Container(
-                              padding: EdgeInsets.symmetric(vertical: 12.0), // Increased vertical padding for button
-                              width: 200,
-                              decoration: BoxDecoration(
-                                color: Color(0xffff5720),
-                                borderRadius: BorderRadius.circular(20),
+                            SizedBox(height: 30.0),
+                            TextFormField(
+                              controller: useremailcontroller,
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return "Please Enter Password";
+                                }
+                                return null;
+                              },
+                              decoration: InputDecoration(
+                                hintText: "Email",
+                                hintStyle: AppWidget.boldTextFieldStyle(),
+                                prefixIcon: Icon(Icons.email_outlined),
                               ),
-                              child: Center(
-                                child: Text(
-                                  "LOGIN",
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 18.0,
-                                    fontFamily: 'Poppins1',
-                                    fontWeight: FontWeight.bold,
+                            ),
+                            SizedBox(height: 30.0),
+                            TextFormField(
+                              controller: userpasswordcontroller,
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return "Please Enter Password";
+                                }
+                                return null;
+                              },
+                              obscureText: true,
+                              decoration: InputDecoration(
+                                hintText: "Password",
+                                hintStyle: AppWidget.boldTextFieldStyle(),
+                                prefixIcon: Icon(Icons.password_outlined),
+                              ),
+                            ),
+                            SizedBox(height: 30.0),
+                            Container(
+                              alignment: Alignment.topRight,
+                              child: Text(
+                                "Forgot password?",
+                                style: AppWidget.boldTextFieldStyle(),
+                              ),
+                            ),
+                            SizedBox(
+                                height: 50.0), // Reduced space above the button
+                            GestureDetector(
+                              onTap: () {
+                                if (_formkey.currentState!.validate()) {
+                                  setState(() {
+                                    email = useremailcontroller.text;
+                                    password = userpasswordcontroller.text;
+                                  });
+                                }
+                                UserLogin();
+                              },
+                              child: Material(
+                                elevation: 5.0,
+                                borderRadius: BorderRadius.circular(20),
+                                child: Container(
+                                  padding: EdgeInsets.symmetric(
+                                      vertical:
+                                          12.0), // Increased vertical padding for button
+                                  width: 200,
+                                  decoration: BoxDecoration(
+                                    color: Color(0xffff5720),
+                                    borderRadius: BorderRadius.circular(20),
+                                  ),
+                                  child: Center(
+                                    child: Text(
+                                      "LOGIN",
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 18.0,
+                                        fontFamily: 'Poppins1',
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
                                   ),
                                 ),
                               ),
-                            ),
-                          )
-                        ],
+                            )
+                          ],
+                        ),
                       ),
                     ),
                   ),
