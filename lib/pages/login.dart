c2eda802e1b45e7e4354bc4762664ac3c664a687
AzzamHussain <azzamhussain70@gmail.com>
2024-08-23 02:35:05 +0500

    error resolved

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -13,99 +13,127 @@ class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Container(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height / 2.5,
-            decoration: BoxDecoration(
-                gradient: LinearGradient(
-                    begin: Alignment.topLeft,
-                    end: Alignment.bottomRight,
-                    colors: [Color(0xFFff5c30), Color(0xFFe74b1a)])),
-          ),
-          Container(
-            margin: EdgeInsets.only(
-              top: MediaQuery.of(context).size.height / 3,
+      body: SingleChildScrollView(
+        child: Stack(
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height / 2.5,
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                      colors: [Color(0xFFff5c30), Color(0xFFe74b1a)])),
             ),
-            height: MediaQuery.of(context).size.height / 2,
-            width: MediaQuery.of(context).size.width,
-            decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(40.0),
-                    topRight: Radius.circular(40.0))),
-            child: Text(""),
-          ),
-          Container(
-            margin: EdgeInsets.only(top: 60.0,left: 20.0,right: 20.0), //ad here left right
-            child: Column(
-              children: [
-                Center(
-                  child: 
-                    Image.asset(
+            Container(
+              margin: EdgeInsets.only(
+                top: MediaQuery.of(context).size.height / 3,
+              ),
+              height: MediaQuery.of(context).size.height / 2,
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(40.0),
+                      topRight: Radius.circular(40.0))),
+              child: Text(""),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 60.0, left: 20.0, right: 20.0),
+              child: Column(
+                children: [
+                  Center(
+                    child: Image.asset(
                       "assets/logo.png",
                       width: MediaQuery.of(context).size.width / 2,
                       fit: BoxFit.cover,
-                  
-                    ),),
-                    SizedBox(height: 50.0,),
-
-                    Material(
-                      elevation: 5.0,
-                      borderRadius: BorderRadius.circular(10.0),
-                      child: Container(
-                        padding: EdgeInsets.only(right: 20.0,left: 20.0),
-                        width: MediaQuery.of(context).size.width,
-                        height:  MediaQuery.of(context).size.height/1.5,
-                      
-                        decoration: BoxDecoration(color: Colors.white,borderRadius: BorderRadius.circular(10.0)),
-                        child: Column(children: [
-                          SizedBox(height: 30.0,),
-                          
-                          Text("Login",style: AppWidget.HeadLineTextFieldStyle(),),
-                          SizedBox(height: 30.0,),
-
-                          TextField(
-                            decoration: InputDecoration(hintText: "Email",hintStyle: AppWidget.boldTextFieldStyle(),
-                            prefixIcon: Icon(Icons.email_outlined) ),),
-                            SizedBox(height: 30.0),
-                            TextField(
-                            decoration: InputDecoration(hintText: "Password",hintStyle: AppWidget.boldTextFieldStyle(),
-                            prefixIcon: Icon(Icons.password_outlined) ),),
-                            SizedBox(height:30.0),
-                            Container(
-                             
-                              alignment:Alignment.topRight, 
-                              child :Text("Forgot password?",style: AppWidget.boldTextFieldStyle(),)),
-                              SizedBox(height:  60.0),
-                              Material(
-                                elevation: 5.0,
-                              borderRadius: BorderRadius.circular(20),
-                                child: Container(
-                                   padding: EdgeInsets.symmetric(vertical: 8.0),
-                                width: 200,
-                                  decoration: BoxDecoration(color: Color(0xffff5720), borderRadius: BorderRadius.circular(20),),
-
-                                  child: Center(child: Text("LOGIN",style: TextStyle(color: Colors.white,fontSize: 18.0,fontFamily: 'Poppins1',fontWeight: FontWeight.bold),)),
-                                ),
-                              )     
-                            ],
-                            ),
-                      ),
-                      
                     ),
-                   SizedBox( height: 50.0,),
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.push(context, MaterialPageRoute(builder: (context)=>SignUp()));
-                      },
-                      child: Text("Don't have an account? SignUp",style:AppWidget.boldTextFieldStyle(),))              
-                
-              ],
+                  ),
+                  SizedBox(height: 50.0),
+                  Material(
+                    elevation: 5.0,
+                    borderRadius: BorderRadius.circular(10.0),
+                    child: Container(
+                      padding: EdgeInsets.only(right: 20.0, left: 20.0),
+                      width: MediaQuery.of(context).size.width,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10.0)),
+                      child: Column(
+                        children: [
+                          SizedBox(height: 30.0),
+                          Text(
+                            "Login",
+                            style: AppWidget.HeadLineTextFieldStyle(),
+                          ),
+                          SizedBox(height: 30.0),
+                          TextField(
+                            decoration: InputDecoration(
+                              hintText: "Email",
+                              hintStyle: AppWidget.boldTextFieldStyle(),
+                              prefixIcon: Icon(Icons.email_outlined),
+                            ),
+                          ),
+                          SizedBox(height: 30.0),
+                          TextField(
+                            decoration: InputDecoration(
+                              hintText: "Password",
+                              hintStyle: AppWidget.boldTextFieldStyle(),
+                              prefixIcon: Icon(Icons.password_outlined),
+                            ),
+                          ),
+                          SizedBox(height: 30.0),
+                          Container(
+                            alignment: Alignment.topRight,
+                            child: Text(
+                              "Forgot password?",
+                              style: AppWidget.boldTextFieldStyle(),
+                            ),
+                          ),
+                          SizedBox(height: 60.0),
+                          Material(
+                            elevation: 5.0,
+                            borderRadius: BorderRadius.circular(20),
+                            child: Container(
+                              padding: EdgeInsets.symmetric(vertical: 8.0),
+                              width: 200,
+                              decoration: BoxDecoration(
+                                color: Color(0xffff5720),
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                              child: Center(
+                                child: Text(
+                                  "LOGIN",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 18.0,
+                                    fontFamily: 'Poppins1',
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 50.0),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => SignUp()));
+                    },
+                    child: Text(
+                      "Don't have an account? SignUp",
+                      style: AppWidget.boldTextFieldStyle(),
+                    ),
+                  )
+                ],
+              ),
             ),
-          )
-        ],
+          ],
+        ),
       ),
     );
   }
--- lib/pages/signup.dart
+++ lib/pages/signup.dart
@@ -14,11 +14,9 @@ class SignUp extends StatefulWidget {
 class _SignUpState extends State<SignUp> {
   String email = "", password = "", name = "";
 
-  TextEditingController namecontroller = new TextEditingController();
-
-  TextEditingController passwordcontroller = new TextEditingController();
-
-  TextEditingController mailcontroller = new TextEditingController();
+  TextEditingController namecontroller = TextEditingController();
+  TextEditingController passwordcontroller = TextEditingController();
+  TextEditingController mailcontroller = TextEditingController();
 
   final _formkey = GlobalKey<FormState>();
 
@@ -34,20 +32,7 @@ class _SignUpState extends State<SignUp> {
               "Registered Successfully",
               style: TextStyle(fontSize: 20.0),
             ))));
-        // String Id = randomAlphaNumeric(10);
-        // Map<String, dynamic> addUserInfo = {
-        //   "Name": namecontroller.text,
-        //   "Email": mailcontroller.text,
-        //   "Wallet": "0",
-        //   "Id": Id,
-        // };
-        // await DatabaseMethods().addUserDetail(addUserInfo, Id);
-        // await SharedPreferenceHelper().saveUserName(namecontroller.text);
-        // await SharedPreferenceHelper().saveUserEmail(mailcontroller.text);
-        // await SharedPreferenceHelper().saveUserWallet('0');
-        // await SharedPreferenceHelper().saveUserId(Id);
 
-        // ignore: use_build_context_synchronously
         Navigator.pushReplacement(
             context, MaterialPageRoute(builder: (context) => Homepage()));
       } on FirebaseException catch (e) {
@@ -62,7 +47,7 @@ class _SignUpState extends State<SignUp> {
           ScaffoldMessenger.of(context).showSnackBar(SnackBar(
               backgroundColor: Colors.orangeAccent,
               content: Text(
-                "Account Already exsists",
+                "Account Already exists",
                 style: TextStyle(fontSize: 18.0),
               )));
         }
@@ -73,53 +58,45 @@ class _SignUpState extends State<SignUp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Stack(
-          children: [
-            Container(
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height / 2.5,
-              decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                      begin: Alignment.topLeft,
-                      end: Alignment.bottomRight,
-                      colors: [
-                    Color(0xFFff5c30),
-                    Color(0xFFe74b1a),
-                  ])),
-            ),
-            Container(
-              margin:
-                  EdgeInsets.only(top: MediaQuery.of(context).size.height / 3),
-              height: MediaQuery.of(context).size.height / 2,
-              width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(40),
-                      topRight: Radius.circular(40))),
-              child: Text(""),
-            ),
-            Container(
-              margin: EdgeInsets.only(top: 60.0, left: 20.0, right: 20.0),
-              child: Column(
-                children: [
-                  Center(
-                      child: Image.asset(
-                    "assets/logo.png",
-                    width: MediaQuery.of(context).size.width / 1.5,
-                    fit: BoxFit.cover,
-                  )),
-                  SizedBox(
-                    height: 50.0,
+      resizeToAvoidBottomInset: false, // Prevents automatic resize
+      body: Stack(
+        children: [
+          // Background gradient
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height / 2.5,
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                    colors: [
+                  Color(0xFFff5c30),
+                  Color(0xFFe74b1a),
+                ])),
+          ),
+          // Form container with scroll view
+          SingleChildScrollView(
+            child: Column(
+              children: [
+                Container(
+                  margin: EdgeInsets.only(top: 60.0, left: 20.0, right: 20.0),
+                  child: Center(
+                    child: Image.asset(
+                      "assets/logo.png",
+                      width: MediaQuery.of(context).size.width / 1.5,
+                      fit: BoxFit.cover,
+                    ),
                   ),
-                  Material(
+                ),
+                SizedBox(height: 20.0),
+                Container(
+                  margin: EdgeInsets.only(top: 10.0, left: 20.0, right: 20.0),
+                  child: Material(
                     elevation: 5.0,
                     borderRadius: BorderRadius.circular(20),
                     child: Container(
                       padding: EdgeInsets.only(left: 20.0, right: 20.0),
                       width: MediaQuery.of(context).size.width,
-                      height: MediaQuery.of(context).size.height / 1.8,
                       decoration: BoxDecoration(
                           color: Colors.white,
                           borderRadius: BorderRadius.circular(20)),
@@ -127,16 +104,12 @@ class _SignUpState extends State<SignUp> {
                         key: _formkey,
                         child: Column(
                           children: [
-                            SizedBox(
-                              height: 30.0,
-                            ),
+                            SizedBox(height: 30.0),
                             Text(
                               "Sign up",
                               style: AppWidget.HeadLineTextFieldStyle(),
                             ),
-                            SizedBox(
-                              height: 30.0,
-                            ),
+                            SizedBox(height: 30.0),
                             TextFormField(
                               controller: namecontroller,
                               validator: (value) {
@@ -150,9 +123,7 @@ class _SignUpState extends State<SignUp> {
                                   hintStyle: AppWidget.boldTextFieldStyle(),
                                   prefixIcon: Icon(Icons.person_outlined)),
                             ),
-                            SizedBox(
-                              height: 30.0,
-                            ),
+                            SizedBox(height: 30.0),
                             TextFormField(
                               controller: mailcontroller,
                               validator: (value) {
@@ -166,9 +137,7 @@ class _SignUpState extends State<SignUp> {
                                   hintStyle: AppWidget.boldTextFieldStyle(),
                                   prefixIcon: Icon(Icons.email_outlined)),
                             ),
-                            SizedBox(
-                              height: 30.0,
-                            ),
+                            SizedBox(height: 30.0),
                             TextFormField(
                               controller: passwordcontroller,
                               validator: (value) {
@@ -183,9 +152,7 @@ class _SignUpState extends State<SignUp> {
                                   hintStyle: AppWidget.boldTextFieldStyle(),
                                   prefixIcon: Icon(Icons.password_outlined)),
                             ),
-                            SizedBox(
-                              height: 80.0,
-                            ),
+                            SizedBox(height: 30.0),
                             GestureDetector(
                               onTap: () async {
                                 if (_formkey.currentState!.validate()) {
@@ -194,14 +161,14 @@ class _SignUpState extends State<SignUp> {
                                     name = namecontroller.text;
                                     password = passwordcontroller.text;
                                   });
+                                  registration();
                                 }
-                                registration();
                               },
                               child: Material(
                                 elevation: 5.0,
                                 borderRadius: BorderRadius.circular(20),
                                 child: Container(
-                                  padding: EdgeInsets.symmetric(vertical: 8.0),
+                                  padding: EdgeInsets.symmetric(vertical: 12.0),
                                   width: 200,
                                   decoration: BoxDecoration(
                                       color: Color(0Xffff5722),
@@ -218,28 +185,31 @@ class _SignUpState extends State<SignUp> {
                                 ),
                               ),
                             ),
+                            SizedBox(height: 20.0),
+                            GestureDetector(
+                              onTap: () {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => Login()));
+                              },
+                              child: Text(
+                                "Already have an account? Login",
+                                style: AppWidget.boldTextFieldStyle(),
+                              ),
+                            ),
+                            SizedBox(height: 20.0),
                           ],
                         ),
                       ),
                     ),
                   ),
-                  SizedBox(
-                    height: 70.0,
-                  ),
-                  GestureDetector(
-                      onTap: () {
-                        Navigator.push(context,
-                            MaterialPageRoute(builder: (context) => Login()));
-                      },
-                      child: Text(
-                        "Already have an account? Login",
-                        style: AppWidget.boldTextFieldStyle(),
-                      ))
-                ],
-              ),
-            )
-          ],
-        ),
+                ),
+                SizedBox(height: 20.0),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
