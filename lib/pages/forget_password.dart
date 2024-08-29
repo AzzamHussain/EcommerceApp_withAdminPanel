a5dc030c0b5b18d4e39ff25e4828240af839c110
AzzamHussain <azzamhussain70@gmail.com>
2024-08-29 23:11:36 +0500

    forget password done with functionality

--- lib/pages/forget_password.dart
+++ lib/pages/forget_password.dart
@@ -1,4 +1,5 @@
 import 'package:ecommerce_with_adminpanel/pages/signup.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class ForgetPassword extends StatefulWidget {
@@ -10,6 +11,28 @@ class ForgetPassword extends StatefulWidget {
 
 class _ForgetPasswordState extends State<ForgetPassword> {
   TextEditingController mailcontroller = new TextEditingController();
+  String email = "";
+  final _formkey = GlobalKey<FormState>();
+
+  resetpassword() async {
+    try {
+      await FirebaseAuth.instance.sendPasswordResetEmail(email: email);
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          content: Text(
+        "Password Reset has been sent!",
+        style: TextStyle(fontSize: 18.0),
+      )));
+    } on FirebaseException catch (e) {
+      if (e.code == 'User-Not-Found') {
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            content: Text(
+          "No User found for that Email!",
+          style: TextStyle(fontSize: 18.0),
+        )));
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,85 +65,98 @@ class _ForgetPasswordState extends State<ForgetPassword> {
             ),
             Expanded(
               child: Form(
+                  key: _formkey,
                   child: Padding(
-                padding: EdgeInsets.only(left: 10.0),
-                child: ListView(
-                  children: [
-                    Container(
-                      padding: EdgeInsets.only(left: 10.0),
-                      decoration: BoxDecoration(
-                          border: Border.all(color: Colors.white70, width: 2.0),
-                          borderRadius: BorderRadius.circular(30)),
-                      child: TextFormField(
-                          controller: mailcontroller,
-                          validator: (value) {
-                            if (value == null || value.isEmpty) {
-                              return "Please Enter Email";
-                            }
-                            return null;
-                          },
-                          style: TextStyle(color: Colors.white),
-                          decoration: InputDecoration(
-                              hintText: "Email",
-                              hintStyle: TextStyle(
-                                  fontSize: 18.0, color: Colors.white),
-                              prefixIcon: Icon(
-                                Icons.person,
-                                color: Colors.white,
-                                size: 30.0,
-                              ),
-                              border: InputBorder.none)),
-                    ),
-                    SizedBox(
-                      height: 40.0,
-                    ),
-                    Container(
-                        width: 140,
-                        padding: EdgeInsets.all(10.0),
-                        decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(10)),
-                        child: Center(
-                          child: Text(
-                            "Send Email",
-                            style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 18.0,
-                                fontWeight: FontWeight.bold),
-                          ),
-                        )),
-                    SizedBox(
-                      height: 50.0,
-                    ),
-                    Row(
+                    padding: EdgeInsets.only(left: 10.0),
+                    child: ListView(
                       children: [
-                        Text(
-                          "Dont Have an account?",
-                          style: TextStyle(fontSize: 18.0, color: Colors.white),
+                        Container(
+                          padding: EdgeInsets.only(left: 10.0),
+                          decoration: BoxDecoration(
+                              border:
+                                  Border.all(color: Colors.white70, width: 2.0),
+                              borderRadius: BorderRadius.circular(30)),
+                          child: TextFormField(
+                              controller: mailcontroller,
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return "Please Enter Email";
+                                }
+                                return null;
+                              },
+                              style: TextStyle(color: Colors.white),
+                              decoration: InputDecoration(
+                                  hintText: "Email",
+                                  hintStyle: TextStyle(
+                                      fontSize: 18.0, color: Colors.white),
+                                  prefixIcon: Icon(
+                                    Icons.person,
+                                    color: Colors.white,
+                                    size: 30.0,
+                                  ),
+                                  border: InputBorder.none)),
                         ),
                         SizedBox(
-                          width: 5.0,
+                          height: 40.0,
                         ),
                         GestureDetector(
                           onTap: () {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => SignUp()));
+                            if (_formkey.currentState!.validate()) {
+                              setState(() {
+                                email = mailcontroller.text;
+                              });
+                              resetpassword();
+                            }
                           },
-                          child: Text(
-                            "Create",
-                            style: TextStyle(
-                                color: Colors.yellow,
-                                fontSize: 20.0,
-                                fontWeight: FontWeight.w500),
-                          ),
+                          child: Container(
+                              width: 140,
+                              padding: EdgeInsets.all(10.0),
+                              decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(10)),
+                              child: Center(
+                                child: Text(
+                                  "Send Email",
+                                  style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 18.0,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              )),
                         ),
+                        SizedBox(
+                          height: 50.0,
+                        ),
+                        Row(
+                          children: [
+                            Text(
+                              "Dont Have an account?",
+                              style: TextStyle(
+                                  fontSize: 18.0, color: Colors.white),
+                            ),
+                            SizedBox(
+                              width: 5.0,
+                            ),
+                            GestureDetector(
+                              onTap: () {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => SignUp()));
+                              },
+                              child: Text(
+                                "Create",
+                                style: TextStyle(
+                                    color: Colors.yellow,
+                                    fontSize: 20.0,
+                                    fontWeight: FontWeight.w500),
+                              ),
+                            ),
+                          ],
+                        )
                       ],
-                    )
-                  ],
-                ),
-              )),
+                    ),
+                  )),
             ),
           ],
         ),
